7aab63bd6e5f3ee7166b3f941e8b5053576792d3
Max Weaver <max.weaver@biomodal.com>
2024-11-26 15:41:50 +0000

    PE-2301: adding arn for all object access for new bucket

--- iam.tf
+++ iam.tf
@@ -38,7 +38,9 @@ data "aws_iam_policy_document" "main" {
         "s3:ListBucket",
       ]
 
-      resources = [module.s3-bucket[0].s3_bucket_arn]
+      resources = [
+        "arn:aws:s3:::*/*",
+      module.s3-bucket[0].s3_bucket_arn, ]
     }
   }
 
